761cddd8dfb6fd6c81bf439c1592b1d78b2e7df7
Sachinthya-M <ms26906430@my.sliit.lk>
2026-03-25 08:25:00 +0530

    feat: add search, filter and pagination support for events API

--- src/events/events.controller.ts
+++ src/events/events.controller.ts
@@ -2,11 +2,12 @@ import { Controller, Post, Body, Get, Param, Put, Delete } from '@nestjs/common'
 import { EventsService } from './events.service';
 import { CreateEventDto } from './dto/create-event.dto';
 import { Event } from './event.entity';
+import { Query } from '@nestjs/common';
 
 @Controller('events')
 export class EventsController {
 
-  constructor(private readonly eventsService: EventsService) {}
+  constructor(private readonly eventsService: EventsService) { }
 
   @Post()
   createEvent(@Body() createEventDto: CreateEventDto): Promise<Event> {
@@ -14,24 +15,30 @@ export class EventsController {
   }
 
   @Get()
-  findAll(): Promise<Event[]> {
-    return this.eventsService.findAll();
+  findAll(
+    @Query('search') search: string,
+    @Query('status') status: string,
+    @Query('date') date: string,
+    @Query('page') page: number = 1,
+    @Query('limit') limit: number = 10,
+  ) {
+    return this.eventsService.findAll(search, status, date, page, limit);
   }
 
   @Get(':id')
   findOne(@Param('id') id: string): Promise<Event | null> {
-      return this.eventsService.findOne(id);
+    return this.eventsService.findOne(id);
   }
   @Put(':id')
   updateEvent(
     @Param('id') id: string,
     @Body() updateEventDto: CreateEventDto): Promise<Event> {
-      return this.eventsService.updateEvent(id, updateEventDto);
+    return this.eventsService.updateEvent(id, updateEventDto);
   }
   @Delete(':id')
   deleteEvent(@Param('id') id: string) {
     return this.eventsService.deleteEvent(id);
-}
+  }
 
 
 
